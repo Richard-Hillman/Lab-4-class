a29a10d463fe5e0b9ac2f8cdb0dfafb6cbc640eb
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-12-03 17:36:15 -0800

    mini server set up successfully hit postman

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 
 // method for express to recognize incoming request as a JSON object, comes from express-------------------------------------------
-app.use(express.json());
+// app.use(express.json());
 
 // endpoints ------------------------------------
-
+app.get('/', (req, res) => {
+    res.send('YOU DIT IT');
+});
 
 
 // listen ----------------------------------------
