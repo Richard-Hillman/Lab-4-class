5309f491840c94694bc526c82e3bc91df2a290e8
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-12-03 17:55:56 -0800

    basic stack foundation created

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
+const Linter = require('./Linter');
 
 // middleware, method for express to recognize incoming request as a JSON object, comes from express-------------------------------------------
 app.use(express.json());
 
 // endpoints ------------------------------------
 app.post('/api/v1/linter', (req, res) => {
+    const newLintedText = Linter(req.body)
     console.log(req.body);
-    res.send('YOU DIT IT');
+    res.send(newLintedText)
+    res.send('Linted!');
 });
 
 
